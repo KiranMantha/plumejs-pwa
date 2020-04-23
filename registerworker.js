a16bd7035a1686ccd8770eb701697d46e9cc9db2
Kiran Mantha <kiranv.mantha@gmail.com>
2020-04-23 21:39:52 +0530

    Add files via upload

---
+++ registerworker.js
@@ -0,0 +1,51 @@
+function notifyMe(message) {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(message || "Hi there!");
+      }
+    }).then(function (reg) {
+      return reg.sync.register('syncTest');
+    }).then(function () {
+      console.log('Sync registered');
+    }).catch(function (err) {
+      console.log('It broke');
+      console.log(err.message);
+    });
+  }
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
+// Registering our Service worker
+window.addEventListener('load', function () {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js', {
+      scope: './'
+    });
+    notifyMe();
+  }
+})
+
+// prompt for pwa install
+window.addEventListener('beforeinstallprompt',function (e) {
+  // log the platforms provided as options in an install prompt 
+  console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+  e.userChoice.then(function (choiceResult) {
+    console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+  }, handleError);
+});
---
+++ sw.js
@@ -0,0 +1,119 @@
+var VERSION = "static-v1";
+
+var cacheFirstFiles = [
+  "/index.html",
+  "/3.chunk.js",
+  "/4.chunk.js",
+  "/5.chunk.js",
+  "/6.chunk.js",
+  "/main.chunk.js",
+  "/vendors-main.chunk.js",
+  "/runtime.bundle.js",
+];
+
+//var cacheFirstFiles = [];
+
+var networkFirstFiles = [];
+
+// Below is the service worker code.
+
+var cacheFiles = cacheFirstFiles.concat(networkFirstFiles);
+
+self.oninstall = function (event) {
+  event.waitUntil(
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  );
+};
+
+function removeCache() {
+  return caches.keys().then(function (cacheNames) {
+    return Promise.all(
+      cacheNames.map(function (cacheName) {
+        console.log('cachename', cacheName);
+        if (VERSION !== cacheName) {
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  });
+}
+
+function fromNetwork(request) {
+  return fetch(request).then(function (response) {
+    return response;
+  });
+}
+
+function fromCache(request) {
+  return caches.open(VERSION).then(function (cache) {
+    return new Promise(function (resolve, reject) {
+      cache.match(request).then(function (matching) {
+        if (matching) {
+          resolve(matching);
+        } else {
+          reject('no-entry');
+        }
+      });
+    });
+  });
+}
+
+function update(request) {
+  return caches.open(VERSION).then(function (cache) {
+    return fetch(request).then(function (response) {
+      if (request.method !== 'POST') {
+        cache.put(request, response.clone());
+      }
+      return response;
+    });
+  });
+}
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    if (response) {
+      clients.forEach(function (client) {
+        var message = {
+          type: 'refresh',
+          url: response.url,
+          eTag: response.headers.get('ETag')
+        };
+        client.postMessage(JSON.stringify(message));
+      });
+    }
+  });
+}
+
+self.onactivate = function (event) {
+  if (navigator.onLine) {
+    event.waitUntil(removeCache())
+  }
+}
+
+// check cache first if not found make network request
+self.onfetch = function (evt) {
+  evt.respondWith(fromCache(evt.request).catch(function () {
+    return fromNetwork(evt.request).catch(update(evt.request).then(refresh));
+  }));
+}
+
+// // prompt for pwa install
+// self.onbeforeinstallprompt = function (e) {
+//   // log the platforms provided as options in an install prompt 
+//   console.log(e.platforms); // e.g., ["web", "android", "windows"] 
+//   e.userChoice.then(function (choiceResult) {
+//     console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+//   }, handleError);
+// }
+
+// to send message from chrome debugging tools
+self.onpush = function (event) {
+  self.registration.showNotification(event.data.text());
+}
+
+// register sync event
+self.onsync = function (event) {
+  self.registration.showNotification(event.tag);
+}
